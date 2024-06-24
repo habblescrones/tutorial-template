3dfba53e3c32cd3acef9c55d8a81f8c4180d102c
habblescrones <95175364+habblescrones@users.noreply.github.com>
2024-06-23 23:06:55 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Adventure Game Engine'
+copyright = '2024 habblescrones LTD'
+author = 'habblescrones'
 
 release = '0.1'
 version = '0.1.0'
